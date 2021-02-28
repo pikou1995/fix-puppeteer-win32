9e0a72cb03a7ec059d8ef26fcfcb6272b6b996b4
pikou <434708082@qq.com>
2021-02-28 15:16:11 +0800

    add npx bin script

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-fix puppeteer on win32
+# fix-puppeteer-win32
+
+this package can fix chromium on win32 and has no side effects on other platform.
+
+## Usage
+
+This package registered npm postinstall hook, will fix puppeteer's .local-chromium after installed.
+
+```
+npm i fix-puppeteer-win32
+```
+
+You can install this package globally and fix package with `npx fix-puppeteer-win32`
+
+```
+npm i -g fix-puppeteer-win32
+cd <your package>
+npx fix-puppeteer-win32
+```
+
+or
+
+```
+npm i -g fix-puppeteer-win32
+npx fix-puppeteer-win32 <custom chromium installed path>
+```
--- fix.js
+++ fix.js
@@ -1,8 +1,39 @@
 #!/usr/bin/env node
 const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const execSync = require('child_process').execSync
+
 const platform = os.platform()
 
 if (platform !== 'win32') {
   console.warn(`This package doesn't work on platform: ${platform}`)
   process.exit(0)
 }
+
+const chromiumPath = getChromiumPath()
+
+function getChromiumPath() {
+  const cwd = process.cwd()
+  const customChromiumPath = process.argv[2]
+  if (customChromiumPath) {
+    return path.join(cwd, customChromiumPath)
+  }
+
+  const puppeteerChromiumPath = path.join(
+    cwd,
+    'node_modules/puppeteer/.local-chromium'
+  )
+  return path.join(
+    puppeteerChromiumPath,
+    fs.readdirSync(puppeteerChromiumPath)[0],
+    'chrome-win'
+  )
+}
+
+if (!fs.existsSync(chromiumPath)) {
+  console.warn(`No chromium found in ${chromiumPath}.`)
+  process.exit(0)
+}
+
+execSync(`fix.cmd ${chromiumPath}`)
